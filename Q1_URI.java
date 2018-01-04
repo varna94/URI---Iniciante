7f8ee3fa275cff73763938b0037baf3c74182439
varna94 <jessicakelly352@gmail.com>
2018-01-04 21:40:40 -0200

    Add files via upload

---
+++ Q1_URI.java
@@ -0,0 +1,25 @@
+
+package q1_uri;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Q1_URI {
+
+   
+    public static void main(String[] args) throws IOException{
+        
+        
+//            String arg = args[0];
+            Scanner valor = new Scanner(System.in);
+            int A = valor.nextInt() ;
+            int B = valor.nextInt();
+            int X = A + B;
+        System.out.print("X="+X+"\n"); 
+        
+        
+        
+    }
+    
+}
